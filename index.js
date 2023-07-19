42f7dbbc08476a7d829bfa484c6e826df9295bba
Samuel_Ramos <progsamu@gmail.com>
2023-07-19 10:03:58 -0300

    Atvd final

---
+++ index.js
@@ -0,0 +1,169 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const usuariosCadastrados = [];
+let idUsuario = 0;
+const recados = []
+let idRecado = 0;
+
+//  ROTA CRIAR USUÁRIO - ok
+app.post("/cadastrar-usuario",(req, res) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const novousuario = { nome, email, senha, idUsuario, recados };
+
+  if (nome === undefined || email === undefined || senha === undefined) {
+    res.send(`Insira um dado válido. Veja o exemplo abaixo:
+
+              { 
+                "nome":"exemplo", 
+                "email": "exemplo@email.com",
+                "senha": "senha"
+              }`);
+  } 
+
+ const emailEncontrado = usuariosCadastrados.find((usuario) => usuario.email === email);
+
+ if (emailEncontrado) {
+  res.send(`O e-mail: ${novousuario.email} já está cadastrado`)
+ } else {
+  usuariosCadastrados.push(novousuario);
+  idUsuario++;
+  res.send('Usuário cadastrado com sucesso')
+ }
+ console.log(usuariosCadastrados);
+ console.log('------------------------------');
+
+});
+
+//ROTA LOGIN - ok
+app.get("/login/:idUsuario", (req, res) => {
+  const idUsuario = parseInt(req.params.idUsuario); 
+
+  const encontrarUsuario = usuariosCadastrados.find((usuario) => usuario.idUsuario === idUsuario);
+
+  const { email, senha } = req.body;
+
+  if (email === undefined || senha === undefined) {
+    res.send(`Tentativa inválida!
+      Forneça o id do usuário após a rota e no body(json) envie o email e a senha:
+
+      {
+        "email": "email@example.com",
+        "senha": "123"
+      }
+    `);
+  } else {
+    if (encontrarUsuario) {
+      if (encontrarUsuario.email === email && encontrarUsuario.senha === senha) {
+        res.send('Login efetuado com sucesso');
+      } else {
+        res.send('Verifique as informações e tente novamente');
+      }
+    } else {
+      res.send('Usuário não encontrado');
+    }
+  }
+});
+
+//ROTA LISTAR USUARIOS CADASTRADOS - ok
+app.get("/cadastrados", (req, res)=> {
+  res.json(usuariosCadastrados)
+})
+
+//ROTA  CRIAR RECADOS - ok 
+app.post("/criarRecado/:idUsuario", (req, res)=> {
+  const idUsuario = parseInt(req.params.idUsuario); 
+  const encontrarUsuario = usuariosCadastrados.find((usuario) => usuario.idUsuario === idUsuario);
+
+  if(!encontrarUsuario){ res.send('Usuário não encontrado')}
+  const titulo = req.body.titulo
+  const descricao = req.body.descricao
+  const novoRecado = { titulo, descricao, idRecado, idUsuario}
+
+  if (titulo === undefined || descricao === undefined) {
+    res.send(`Insira um dado válido. Veja o exemplo abaixo:
+
+              { 
+                "titulo":"titulo1", 
+                "descricao": "descricao1"
+              }`);
+  } else {
+    idRecado++
+    recados.push(novoRecado)
+    res.send('Novo recado cadastrado com sucesso')
+    console.log(recados);
+    console.log(`----------------------`);
+  }
+})
+
+//Rota para LISTAR recados de um usuario - ok 
+app.get("/recados/:idUsuario", (req, res) => {
+  const idUsuario = parseInt(req.params.idUsuario);
+
+  const usuario = usuariosCadastrados.find((user) => user.idUsuario === idUsuario);
+  const recadosDoUsuario = recados.filter((recado) => recado.idUsuario === idUsuario);
+
+  if (!usuario) {
+    res.status(404).json({ error: "Usuario não encontrado" });
+  } else {
+    res.status(200).json({ mensagem: "Recados encontrados", recados: recadosDoUsuario });
+  }
+});
+
+//Rota para editar um recado pelo ID -ok
+app.put("/recados/:idUsuario/:idRecado", (req, res)=>{
+  const userId = req.params.idUsuario;
+  const user = usuariosCadastrados.find((user) => user.idUsuario === parseInt(userId));
+  if (!user) {
+    res.status(404);
+    res.send({ error: "Usuario não encontrado" });
+  }
+
+  const id = req.params.idRecado;
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  const recado = user.recados.find((recado) => recado.idRecado === parseInt(id));
+
+  if (!recado) {
+    res.status(404);
+    res.send({ error: "Recado não encontrado" });
+  }
+
+  recado.titulo = titulo || recado.titulo;
+  recado.descricao = descricao || recado.descricao;
+
+  res.send({ mensagem: "Recado alterado", recado: recado });
+})
+
+//Rota para deletar um recado pelo ID - ok
+app.delete("/recados/:idUsuario/:idRecado", (req, res) => {
+  const userId = req.params.idUsuario;
+  const user = usuariosCadastrados.find((user) => user.idUsuario === parseInt(userId));
+
+  if (!user) {
+    res.status(404);
+    res.send({ error: "Usuario não encontrado" });
+    return;
+  }
+
+  const id = req.params.idRecado;
+  const index = user.recados.findIndex((recado) => recado.idRecado === parseInt(id));
+
+  if (index === -1) {
+    res.status(404);
+    res.send({ error: "Recado não encontrado" });
+    return;
+  }
+
+  user.recados.splice(index, 1);
+  res.send("Recado removido com sucesso!" + recados);
+});
+
+
+app.listen(3000, () => {
+  console.log("Acesso a porta 3000 concuído com sucesso");
+});
+
---
+++ tarefas.md
@@ -0,0 +1,43 @@
+# Aplicação CRUD de recados
+
+
+# Criação de conta
+
+    ○ Identificador - [ ok ] Único
+    ○ Nome
+    ○ E-mail 
+    ○ Senha
+
+            => Verificações
+                [ ok ] Nome && E-mail && Senha tem que ser !undefined
+                [ ok ] E-mail !e-mail cadastrado
+
+# Login
+
+    ○ E-mail
+    ○ Senha
+
+             => Verificações
+                [ok] E-mail && Senha tem que ser !undefined
+                [ok] E-mail && Senha === a user cadastrado
+
+# CRUD de recados (Criar, Listar, Atualizar e Apagar)
+
+    ○ Identificador - [ ok ] Único
+    ○ Título
+    ○ Descrição
+
+             => Verificações
+                [ ok ] Título && Descrição !undefined
+                [ ok ] Criar recado
+                [ ] Para cadastrar o recado, o user deve está cadastrado e logado
+                [ ok ] Cada recado deve ser de um único usuário
+                [ ] editar recado
+                [ ] apagar recado
+
+
+
+# Falta
+//Rota para editar um recado pelo ID
+//Rota para deletar um recado pelo ID
+
