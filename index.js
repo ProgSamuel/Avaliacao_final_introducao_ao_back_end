34ad27f62fba518dd530b35543e34866dd6f9a2b
Samuel_Ramos <progsamu@gmail.com>
2023-07-23 16:01:23 -0300

    v - final

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-import { bemVindo } from "./bemVindo";
 
 const usuariosCadastrados = [];
 let idUsuario = 0;
@@ -17,6 +16,50 @@ function verificarLogin(req, res, next) {
     res.status(401).send("Acesso não autorizado. Faça o login primeiro.");
   }
 }
+function bemVindo (){
+  const BemVindo = {"Bem Vindo": "Bem vindo a API Recados!"}
+  const rotas = ["Rotas:"]
+  const conteudo = [ {
+      "Função": "Cadastrar um usuário",
+      "Rota": "/cadastrar-usuario",
+      "Método": "POST"
+    },
+    {
+      "Função": "Fazer login",
+      "Rota": "/login/:idUsuario",
+      "Método": "GET"
+    },
+    {
+      "Função": "Criar um recado",
+      "Rota": "/criarRecado/:idUsuario",
+      "Método": "POST"
+    },
+    {
+      "Função": "Listar recados",
+      "Rota": "/recados/:idUsuario",
+      "Método": "GET"
+    },
+    {
+      "Função": "Editar recado",
+      "Rota": "/recados/:idUsuario/:idRecado",
+      "Método": "PUT"
+    },
+    {
+      "Função": "Deletar recado",
+      "Rota": "/recados/:idUsuario/:idRecado",
+      "Método": "DELETE"
+    },
+    {
+      "Função": "Deletar perfil",
+      "Rota": "/delete/:idUsuario",
+      "Método": "DELETE"
+    },
+    {
+      "Função": "Sair do perfil",
+      "Rota": "/sair/:idUsuario",
+      "Método": "DELETE"
+    }]}
+
 
 app.get("/", (req, res) => {
   res.send(bemVindo())
